28869a02e3ea9956c4f624f4ad2127c534835b03
Manoj Kumar <mk725580@gmail.com>
2022-09-18 17:02:27 +0530

    initial commit

---
+++ app/src/main/java/com/example/wallpaer_app/ActivityClass/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wallpaer_app.ActivityClass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wallpaer_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
